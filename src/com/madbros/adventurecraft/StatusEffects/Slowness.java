e46579fffe7c47d27fecf01e3e898e5d42deee76
Chris <info@ignitionmaine.com>
2014-04-23 14:00:32 -0400

    added hunger bar mechanics

---
+++ src/com/madbros/adventurecraft/StatusEffects/Slowness.java
@@ -0,0 +1,34 @@
+package com.madbros.adventurecraft.StatusEffects;
+
+import com.madbros.adventurecraft.Game;
+import com.madbros.adventurecraft.Time;
+import com.madbros.adventurecraft.GameObjects.Actor;
+
+public class Slowness extends TimedStatusEffect{
+	
+	public Slowness() {
+		//potionEffectTime = 15000;
+		speedAmount = 0.05f;
+		id = 3;
+		timeTriggered = Time.getTime();
+		
+	}
+	
+	public void update(Actor actor) {
+
+	}
+	
+	public void applyEffect() {
+		Game.hero.moveSpeed = Game.hero.moveSpeed - speedAmount;
+		Game.hero.currentSpeed = Game.hero.moveSpeed;
+	}
+	
+	public void eraseEffect() {
+		Game.hero.moveSpeed = Game.hero.moveSpeed + speedAmount;
+		Game.hero.currentSpeed = Game.hero.moveSpeed ;
+		
+		Game.hero.eraseTimedStatusEffect(this.id);
+	}
+	
+	
+}
