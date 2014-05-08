fa8cad7189ff0cce091add9916685066521d19c8
Chris <info@ignitionmaine.com>
2014-05-08 07:41:38 -0400

    Adjusted main state rendering..

--- src/com/madbros/adventurecraft/GameStates/MainState.java
+++ src/com/madbros/adventurecraft/GameStates/MainState.java
@@ -91,13 +91,17 @@ public class MainState extends GameState {
 
 			Game.renderSystem.renderWorld(Game.level);
 			//Game.renderSystem.renderCollectibles(Game.collectibleController);
-			Game.renderSystem.renderParticle(Game.particleEffectController);
+			
 			
 			
 			Game.renderSystem.renderHero(Game.hero, Game.getCenterScreenX() - CHARACTER_SIZE/2, Game.getCenterScreenY() - CHARACTER_SIZE/2);
-			Game.renderSystem.renderWorldAbove(Game.level);
+
 			Game.renderSystem.renderCollectibles(Game.collectibleController);
 			Game.renderSystem.renderMobs(Game.mobController);
+			Game.renderSystem.renderParticle(Game.particleEffectController);
+			Game.renderSystem.renderWorldAbove(Game.level);
+			
+			
 			Game.map.render(Game.level.activeBlocks);
 			
 			
