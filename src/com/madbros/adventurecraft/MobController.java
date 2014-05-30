614b54cf42c797d5ec338207e7b17c635e4020bd
Chris <info@ignitionmaine.com>
2014-05-30 12:21:02 -0400

    fixed mobController thing

--- src/com/madbros/adventurecraft/MobController.java
+++ src/com/madbros/adventurecraft/MobController.java
@@ -84,8 +84,8 @@ public class MobController {
 		int topTile = Game.level.activeBlocks[x][y].getTopTile().id;
 		//System.out.println(topTile);
 		Block[] tileArea = getTileArea(x,y);
-		int num = rand.nextInt(1);//100  //10 is quick
-		if(num == 0 && mobs.size() < 100) { //10
+		int num = rand.nextInt(100);//100  //10 is quick
+		if(num == 0 && mobs.size() < 10) { //10
 			if(possibleSpawnPoint.detectCollision(heroRect)) {
 			//    ***************IF THE WORLD IS DARK	
 			} else if(isDark(possibleSpawnPoint)){
