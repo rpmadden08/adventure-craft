7ab099323d29fc0cb5ede69aab830611d5916d28
Chris <info@ignitionmaine.com>
2014-05-31 14:56:08 -0400

    Inventory Changed back...

--- src/com/madbros/adventurecraft/Inventory.java
+++ src/com/madbros/adventurecraft/Inventory.java
@@ -101,8 +101,8 @@ public class Inventory {
 		invClothing[2] = new ClothingSlot(INV_CHAR_RECT.x +147,INV_CHAR_RECT.y +120,LEGGINGS_SLOT);
 		invClothing[3] = new ClothingSlot(INV_CHAR_RECT.x +147,INV_CHAR_RECT.y +160,BOOTS_SLOT);
 		
-		invBar[0].item = new WoodenShovel();
-		invBar[0].item.stackSize = 1;
+//		invBar[0].item = new WoodenShovel();
+//		invBar[0].item.stackSize = 1;
 //		invBar[1].item = new CopperShovel();
 //		invBar[1].item.stackSize = 1;
 //		invBar[2].item = new Stick();
